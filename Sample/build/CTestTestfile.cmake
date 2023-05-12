310d50aed58f5e7c5fa1468153b826a478649121
tientt010 <bechanhtt010@gmail.com>
2023-05-12 21:39:48 +0700

    sfafs
    test
    
    Co-Authored-By: tuyenlt <85683857+tuyenlt@users.noreply.github.com>

--- Sample/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/e/TuyenLt/LinuxWorkSpace/C/codeptit/Sample
-# Build directory: /mnt/e/TuyenLt/LinuxWorkSpace/C/codeptit/Sample/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
